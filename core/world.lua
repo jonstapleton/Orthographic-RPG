c371db5025a5d5c93244afdbd54306e6fae5a39a
octojon <staplejm@dukes.jmu.edu>
2019-10-12 11:42:50 -0400

    Create world.lua

---
+++ core/world.lua
@@ -0,0 +1,17 @@
+local class = require 'core/middleclass'
+
+local World = class('World')
+
+function World:constructor()
+  self.objs = {}
+end
+
+
+function World:draw()
+
+end
+
+
+function World:sort()
+
+end
